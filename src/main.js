c7de463e7bab89b70ec25d15a8cde724096c1370
fanyoufd <fanyoufd@outbooks.com>
2020-02-29 19:28:29 +0800

    配置组件&搭建路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui' // 引入组件
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 Vue.config.productionTip = false
+
+Vue.use(ElementUI) // 注册全局组件
 
 new Vue({
   router,
