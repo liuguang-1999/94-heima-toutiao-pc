87eab79dd8ffbeb06de6189fedb85ca1ad84f2e2
fanyoufd <fanyoufd@outbooks.com>
2020-03-02 20:01:25 +0800

    配置axios & 赋值给Vue全局属性

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import ElementUI from 'element-ui' // 引入组件
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import '@/style/index.less' // 引入初始化文件
 Vue.config.productionTip = false
-
+const axios = () => import('axios') // 引入网络请求协议包
 Vue.use(ElementUI) // 注册全局组件
-
+Vue.proyotype.$http = axios // 赋值给vue 全局属性
 new Vue({
   router,
   render: h => h(App)
