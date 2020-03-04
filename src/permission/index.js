92d7500c86a4fb33928a845757430c4a67dc0b47
fanyoufd <fanyoufd@outbooks.com>
2020-03-04 08:52:04 +0800

    主页模块-权限&导航守卫拦截

--- src/permission/index.js
+++ src/permission/index.js
@@ -7,7 +7,7 @@ import router from '@/router'
 router.beforeEach(function (to, from, next) {
   // startsWith方法是 已什么字符串开头
   if (to.path.startsWith('/home')) { // 判断是否 直接访问 /home 路径
-    const token = localStorage.getItem('user-token')
+    const token = localStorage.getItem('user-token') // 获取来访者的令牌
     if (!token) {
       next('/login') // 遣返登录页
       return
