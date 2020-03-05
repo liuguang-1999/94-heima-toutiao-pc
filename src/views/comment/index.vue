53239fb27736e80ba5ed5cd97a692f007fce7db7
fanyoufd <fanyoufd@outbooks.com>
2020-03-05 23:16:20 +0800

    搭建-粉丝评论表格框架

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,14 +1,33 @@
 <template>
 <el-card>
+    <!-- 防止面包屑组件 -->
     <bread-crumb slot="header">
-    <span slot="title">评论管理</span>
+    <!-- 评论管理 通过具名插槽给面包屑组件的插槽了 -->
+    <template slot="title">
+     评论管理
+    </template>
     </bread-crumb>
+    <!-- 表格组件 -->
+    <el-table :data="list">
+        <!-- 可以通过 width去设置列宽 -->
+        <el-table-column label="标题" width="600"></el-table-column>
+        <el-table-column label="评论状态"></el-table-column>
+        <el-table-column label="总评论数"></el-table-column>
+        <el-table-column label="粉丝评论数"></el-table-column>
+        <el-table-column label="操作"></el-table-column>
+    </el-table>
 </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      list: [
 
+      ]
+    }
+  }
 }
 </script>
 
