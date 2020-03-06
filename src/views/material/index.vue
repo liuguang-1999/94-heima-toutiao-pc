6915d1246c5984a0146045b7b10b5bd896625868
fanyoufd <fanyoufd@outbooks.com>
2020-03-06 21:46:51 +0800

    素材页面-框架搭建

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -8,12 +8,22 @@
      素材管理
      </template>
     </bread-crumb>
+    <!-- 放置标签页 -->
+    <el-tabs v-model="activeName">
+        <!-- label是 当前页签的名称 name是 选中页签值 -->
+        <el-tab-pane label="全部素材" name="all">全部素材</el-tab-pane>
+        <el-tab-pane label="收藏素材" name="collect">收藏素材</el-tab-pane>
+    </el-tabs>
  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all'
+    }
+  }
 }
 </script>
 
