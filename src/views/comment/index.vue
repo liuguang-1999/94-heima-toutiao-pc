1ae0bb9ea1bf35b5f693578aa7c908fbbad09da5
fanyoufd <fanyoufd@outbooks.com>
2020-03-06 08:18:43 +0800

    评论数据渲染及评论状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -10,11 +10,15 @@
     <!-- 表格组件 -->
     <el-table :data="list">
         <!-- 可以通过 width去设置列宽 -->
-        <el-table-column label="标题" width="600"></el-table-column>
-        <el-table-column label="评论状态"></el-table-column>
-        <el-table-column label="总评论数"></el-table-column>
-        <el-table-column label="粉丝评论数"></el-table-column>
-        <el-table-column label="操作"></el-table-column>
+        <el-table-column prop="title" label="标题" width="600"></el-table-column>
+        <!-- 不能显示布尔值 给这列 -->
+        <el-table-column :formatter="formatterBool" prop="comment_status" label="评论状态"></el-table-column>
+        <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+        <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+        <el-table-column prop="" label="操作">
+          <el-button type="text" size="small">修改</el-button>
+          <el-button type="text" size="small">关闭评论</el-button>
+        </el-table-column>
     </el-table>
 </el-card>
 </template>
@@ -27,6 +31,29 @@ export default {
 
       ]
     }
+  },
+  methods: {
+    getComment () {
+      this.$axios({
+        url: '/articles', // 请求地址
+        params: {
+          response_type: 'comment' // 此参数用来 控制获取数据的类型
+        }
+      }).then(ser => {
+        this.list = ser.data.results
+      })
+    },
+    formatterBool (row, column, cellValue, index) {
+      //  row 代表当前的一行数据
+      // column 代表当前的列信息
+      // cellValue 代表当前单元格的值
+      // index 代表当前的索引
+      // 该函数需要返回一个值 用来显示
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getComment()
   }
 }
 </script>
