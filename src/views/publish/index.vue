033e4339e1346e22bc68aa1eb82c474985bd615f
fanyoufd <fanyoufd@outbooks.com>
2020-03-09 20:39:28 +0800

    发布文章页面--框架基本搭建

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,6 +3,31 @@
     <bread-crumb slot="header">
         <template slot="title">发布文章</template>
     </bread-crumb>
+    <el-form style="margin-left:50px;" label-width="100px">
+      <!-- 标题框 -->
+     <el-form-item label="标题">
+       <!-- 输入框 -->
+      <el-input style="width:60%;" placeholder="请输入标题"></el-input>
+     </el-form-item>
+     <el-form-item label="内容">
+       <el-input type="textarea" :rows="5" placeholder="请输入内容" style="width:40%;"></el-input>
+     </el-form-item>
+     <el-form-item label="封面">
+       <el-radio-group>
+         <el-radio>单图</el-radio>
+         <el-radio>三图</el-radio>
+         <el-radio>无图</el-radio>
+         <el-radio>自动</el-radio>
+       </el-radio-group>
+     </el-form-item>
+     <el-form-item label="频道">
+       <el-select placeholder="请选择频道"></el-select>
+     </el-form-item>
+     <el-form-item label="">
+       <el-button type="primary">发表文章</el-button>
+       <el-button>存入草稿</el-button>
+     </el-form-item>
+    </el-form>
   </el-card>
 </template>
 
