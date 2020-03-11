9aadb74a48f47687fcef873bf38cfc182f740f5f
fanyoufd <fanyoufd@outbooks.com>
2020-03-11 08:15:16 +0800

    账户信息 数据加载赋值给表单数据

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -10,28 +10,53 @@
             <el-form label-width="150px">
                 <el-form-item label="用户名:">
                     <!-- 放置input框显示信息 -->
-                    <el-input style="width:30%;"></el-input>
+                    <el-input style="width:30%;" v-model="formData.name"></el-input>
                 </el-form-item>
                 <el-form-item label="简介:">
-                    <el-input style="width:30%;"></el-input>
+                    <el-input style="width:30%;" v-model="formData.intro"></el-input>
                 </el-form-item>
                 <el-form-item label="邮箱:">
-                    <el-input style="width:30%;"></el-input>
+                    <el-input style="width:30%;" v-model="formData.email"></el-input>
                 </el-form-item>
                 <el-form-item label="手机号:">
-                    <el-input disabled="" style="width:30%;"></el-input>
+                    <el-input disabled="" style="width:30%;" v-model="formData.mobile"></el-input>
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary">保存</el-button>
                 </el-form-item>
             </el-form>
-            <img src="../../assets/home.jpg" alt="" class="head-upload">
+            <!-- 头像信息 -->
+            <img :src="formData.photo ? formData.photo :defaultImg" alt="" class="head-upload">
         </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      formData: { // 表单数据
+        name: '', // 用户名
+        intro: '', // 简介
+        photo: '', // 头像信息
+        email: '', // 邮箱
+        mobile: ''// 手机号
+      },
+      defaultImg: require('../../assets/home.jpg')
+    }
+  },
+  methods: {
+    // 获取公户个人信息
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(ser => {
+        this.formData = ser.data // 请求会的数据 复制给我的 表单数据
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
 }
 </script>
 
